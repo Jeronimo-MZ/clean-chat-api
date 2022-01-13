51549227c8e12bbb96736c5721d1839d47d3dd6b
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-13 13:40:15 +0200

    test: ensure MinLengthValidation returns null if value length is greater than min length

--- tests/validation/validators/MinLengthValidation.spec.ts
+++ tests/validation/validators/MinLengthValidation.spec.ts
@@ -17,10 +17,18 @@ const makeSut = (): SutTypes => {
 };
 
 describe("Min Length Validation", () => {
-    it("should return null if validation succeeds", () => {
+    it("should return null if value length equals min length", () => {
         const { sut } = makeSut();
         const error = sut.validate({
             [field]: faker.random.alphaNumeric(minLength),
+        });
+        expect(error).toBeNull();
+    });
+
+    it("should return an null if value length is greater than min length", () => {
+        const { sut } = makeSut();
+        const error = sut.validate({
+            [field]: faker.random.alphaNumeric(minLength + 1),
         });
         expect(error).toBeNull();
     });
