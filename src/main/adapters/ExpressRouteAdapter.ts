a390daeaf4557c1f938295fcbca3a0d206476d24
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-13 18:34:40 +0200

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/ExpressRouteAdapter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+import { Controller } from "@/presentation/protocols";
+
+export const adaptRoute = (controller: Controller) => {
+    return async (request: Request, response: Response): Promise<Response> => {
+        const req = {
+            ...(request.body || {}),
+        };
+        const { statusCode, body } = await controller.handle(req);
+        if (statusCode >= 400) {
+            return response.status(statusCode).json({ error: body.message });
+        }
+        return response.status(statusCode).json(body);
+    };
+};
