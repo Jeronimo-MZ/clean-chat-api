7803119feed8efcb6ac878b51ce600770184c1cc
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-13 14:50:16 +0200

    test: ensure ValidationComposite returns an error if any validation fails

--- tests/validation/validators/ValidationComposite.spec.ts
+++ tests/validation/validators/ValidationComposite.spec.ts
@@ -1,6 +1,7 @@
 import faker from "@faker-js/faker";
 
 import { ValidationSpy } from "@/tests/validation/mocks";
+import { RequiredFieldError } from "@/validation/errors";
 import { ValidationComposite } from "@/validation/validators";
 
 const field = faker.random.word();
@@ -27,5 +28,12 @@ describe("Validation Composite", () => {
         const { sut } = makeSut();
         const error = sut.validate({ [field]: faker.random.word() });
         expect(error).toBeNull();
+    });
+
+    it("should return an error if any validation fails", () => {
+        const { sut, validationSpies } = makeSut();
+        validationSpies[1].error = new RequiredFieldError(field);
+        const error = sut.validate({ [field]: faker.random.word() });
+        expect(error).toEqual(validationSpies[1].error);
     });
 });
