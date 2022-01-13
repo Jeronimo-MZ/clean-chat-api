8bfe5848a2e047d020949b13195ece705b22528b
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-13 13:10:24 +0200

    feat: ensure RequiredFieldValidation returns RequiredFieldError if validation fails

--- src/validation/validators/RequiredFieldValidation.ts
+++ src/validation/validators/RequiredFieldValidation.ts
@@ -1,10 +1,11 @@
+import { RequiredFieldError } from "@/validation/errors";
 import { Validation } from "@/validation/protocols";
-
-import { RequiredFieldError } from "../errors";
 
 export class RequiredFieldValidation implements Validation {
     constructor(private readonly fieldName: string) {}
-    validate(_input: Validation.Input): RequiredFieldError | null {
-        return null;
+    validate(input: Validation.Input): RequiredFieldError | null {
+        return input[this.fieldName]
+            ? null
+            : new RequiredFieldError(this.fieldName);
     }
 }
