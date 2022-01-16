4d0590c2e84f2d5f6512864a76e68d8e1db6cfc6
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-17 00:37:10 +0200

    feat: ensure JwtAdapter.encrypt returns a token on sign success

--- src/infra/cryptography/JwtAdapter.ts
+++ src/infra/cryptography/JwtAdapter.ts
@@ -5,7 +5,6 @@ import { Encrypter } from "@/data/protocols/cryptography";
 export class JwtAdapter implements Encrypter {
     constructor(private readonly secret: string) {}
     async encrypt(plaintext: string): Promise<string> {
-        await sign({ data: plaintext }, this.secret, { expiresIn: "1d" });
-        return undefined as any;
+        return sign({ data: plaintext }, this.secret, { expiresIn: "1d" });
     }
 }
