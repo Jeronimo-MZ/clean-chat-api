a5d2f9450c818cec5d4a65002f8da779cae2ad99
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-22 23:08:37 +0200

    feat: ensure SearchUsersByUsernameValidationFactory makes ValidationComposite with correctly

---
+++ src/main/factories/controllers/User/SearchUsersByUsernameValidationFactory.ts
@@ -0,0 +1,28 @@
+import { Validation } from "@/validation/protocols";
+import {
+    ValidationBuilder,
+    ValidationComposite,
+} from "@/validation/validators";
+
+export const makeSearchUsersByUsernameValidation = (): Validation => {
+    const usernameValidations = ValidationBuilder.field("username")
+        .required()
+        .username()
+        .build();
+
+    const pageValidations = ValidationBuilder.field("page")
+        .integer()
+        .min(1, { isNumber: true })
+        .build();
+
+    const pageSizeValidations = ValidationBuilder.field("pageSize")
+        .integer()
+        .min(1, { isNumber: true })
+        .build();
+
+    return new ValidationComposite([
+        ...usernameValidations,
+        ...pageValidations,
+        ...pageSizeValidations,
+    ]);
+};
