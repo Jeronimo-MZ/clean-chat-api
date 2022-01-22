a361e466c5cc089ab6841c7dfd6bc6c955275026
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-22 22:37:28 +0200

    feat: ensure IntegerValidation returns an error if an invalid value is given

--- src/validation/validators/IntegerValidation.ts
+++ src/validation/validators/IntegerValidation.ts
@@ -1,9 +1,13 @@
 import { InvalidIntegerError } from "@/validation/errors";
+import { isAbsent } from "@/validation/helpers";
 import { Validation } from "@/validation/protocols";
 
 export class IntegerValidation implements Validation {
     constructor(readonly field: string) {}
-    validate(_input: Validation.Input): InvalidIntegerError | null {
-        return null;
+    validate(input: Validation.Input): InvalidIntegerError | null {
+        return isAbsent(input[this.field]) ||
+            Number.isInteger(input[this.field])
+            ? null
+            : new InvalidIntegerError(this.field);
     }
 }
