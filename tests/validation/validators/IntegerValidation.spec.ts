96567a6cafc26f6f1b2748d2e0c5865c2eea66ae
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-22 22:35:23 +0200

    test: ensure IntegerValidation returns null if a valid integer is provided

---
+++ tests/validation/validators/IntegerValidation.spec.ts
@@ -0,0 +1,22 @@
+import faker from "@faker-js/faker";
+
+import { IntegerValidation } from "@/validation/validators";
+
+type SutTypes = {
+    sut: IntegerValidation;
+};
+
+const field = faker.random.word();
+const makeSut = (): SutTypes => {
+    const sut = new IntegerValidation(field);
+    return { sut };
+};
+
+describe("IntegerValidation", () => {
+    it("should return null if a valid integer is provided", () => {
+        const { sut } = makeSut();
+        expect(
+            sut.validate({ [field]: Math.round(faker.datatype.number()) }),
+        ).toBeNull();
+    });
+});
