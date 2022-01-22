fee9457e2b1d89e37b0267efaf1190353a3986ec
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-22 22:17:27 +0200

    feat: ensure MinValueValidation returns an error if value is lesser than min value

--- src/validation/validators/MinValueValidation.ts
+++ src/validation/validators/MinValueValidation.ts
@@ -1,9 +1,14 @@
 import { MinValueError } from "@/validation/errors";
 import { Validation } from "@/validation/protocols";
 
+import { isAbsent } from "../helpers";
+
 export class MinValueValidation implements Validation {
     constructor(readonly field: string, private readonly minValue: number) {}
-    validate(_input: Validation.Input): MinValueError | null {
-        return null;
+    validate(input: Validation.Input): MinValueError | null {
+        return isAbsent(input[this.field]) ||
+            (input[this.field] as number) >= this.minValue
+            ? null
+            : new MinValueError(this.field, this.minValue);
     }
 }
