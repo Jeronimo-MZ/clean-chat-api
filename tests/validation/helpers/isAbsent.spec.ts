f1e80ed3cdb4b37f5c3cd282ae5c75648eaeae91
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-22 21:58:24 +0200

    test: ensure isAbsent returns false if any valid value is given

--- tests/validation/helpers/isAbsent.spec.ts
+++ tests/validation/helpers/isAbsent.spec.ts
@@ -1,8 +1,17 @@
+import faker from "@faker-js/faker";
+
 import { isAbsent as sut } from "@/validation/helpers";
 
 describe("isAbsent", () => {
     it("should return true if null or undefined is given", async () => {
         expect(sut(undefined)).toBe(true);
         expect(sut(null)).toBe(true);
+    });
+
+    it("should return false if any valid value is given", async () => {
+        expect(sut(faker.random.alphaNumeric())).toBe(false);
+        expect(sut(faker.datatype.number())).toBe(false);
+        expect(sut(faker.datatype.boolean())).toBe(false);
+        expect(sut(0)).toBe(false);
     });
 });
