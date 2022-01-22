65b1276f4b4e10892fadfd679338e1b8c9124ddf
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-22 22:39:00 +0200

    test: ensure IntegerValidation returns an error if an invalid value is given

--- tests/validation/validators/IntegerValidation.spec.ts
+++ tests/validation/validators/IntegerValidation.spec.ts
@@ -1,5 +1,6 @@
 import faker from "@faker-js/faker";
 
+import { InvalidIntegerError } from "@/validation/errors";
 import { IntegerValidation } from "@/validation/validators";
 
 type SutTypes = {
@@ -24,5 +25,13 @@ describe("IntegerValidation", () => {
         const { sut } = makeSut();
         const error = sut.validate({});
         expect(error).toBeNull();
+    });
+
+    it("should return InvalidIntegerError if a float is given", async () => {
+        const { sut } = makeSut();
+        let error = sut.validate({ [field]: Math.random() * 1000 });
+        expect(error).toEqual(new InvalidIntegerError(field));
+        error = sut.validate({ [field]: faker.random.alphaNumeric() });
+        expect(error).toEqual(new InvalidIntegerError(field));
     });
 });
