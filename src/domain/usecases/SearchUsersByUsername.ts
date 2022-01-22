7d5c545b5347206a4c2c9d297bcb1d0b795e8bae
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-22 17:22:04 +0200

    feat: add SearchUsersByUsername usecase

---
+++ src/domain/usecases/SearchUsersByUsername.ts
@@ -0,0 +1,26 @@
+export interface SearchUsersByUsername {
+    search(
+        input: SearchUsersByUsername.Input,
+    ): Promise<SearchUsersByUsername.Output>;
+}
+
+export namespace SearchUsersByUsername {
+    export type Input = {
+        currentUserId: string;
+        username: string;
+        page: number;
+        pageSize: number;
+    };
+
+    export type Output = {
+        users: {
+            id: string;
+            username: string;
+            name: string;
+            avatar?: string;
+        }[];
+        page: number;
+        totalPages: number;
+        pageSize: number;
+    };
+}
--- src/domain/usecases/index.ts
+++ src/domain/usecases/index.ts
@@ -2,3 +2,4 @@ export * from "./AddPrivateRoom";
 export * from "./AddUser";
 export * from "./Authentication";
 export * from "./LoadUserByToken";
+export * from "./SearchUsersByUsername";
