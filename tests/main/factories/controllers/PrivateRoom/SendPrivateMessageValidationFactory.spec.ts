24ae6928ade9816fa2d7833f5e5cea0cb956a757
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-24 18:51:28 +0200

    test: ensure SendPrivateMessageValidationFactory make ValidationComposite with correct validations

---
+++ tests/main/factories/controllers/PrivateRoom/SendPrivateMessageValidationFactory.spec.ts
@@ -0,0 +1,37 @@
+import { ObjectIdValidatorAdapter } from "@/infra/validators/ObjectIdValidatorAdapter";
+import { makeSendPrivateMessageValidation } from "@/main/factories";
+import {
+    ValidationBuilder,
+    ValidationComposite,
+} from "@/validation/validators";
+
+// userId: string;
+// roomId: string;
+// content: string;
+describe("SendPrivateMessageValidationFactory", () => {
+    it("should make ValidationComposite with correct validations", () => {
+        const sendPrivateMessageValidation = makeSendPrivateMessageValidation();
+
+        const objectIdValidator = new ObjectIdValidatorAdapter();
+        const userIdValidations = ValidationBuilder.field("userId")
+            .required()
+            .objectId(objectIdValidator)
+            .build();
+
+        const roomIdValidations = ValidationBuilder.field("roomId")
+            .required()
+            .objectId(objectIdValidator)
+            .build();
+        const contentValidations = ValidationBuilder.field("content")
+            .required()
+            .min(1)
+            .build();
+        expect(sendPrivateMessageValidation).toEqual(
+            new ValidationComposite([
+                ...userIdValidations,
+                ...roomIdValidations,
+                ...contentValidations,
+            ]),
+        );
+    });
+});
