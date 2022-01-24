93efbf0840cd2d57bccc7ab54620908ae409a6ae
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-24 15:11:55 +0200

    test: ensure SocketIoAdapter throws if emit() throws

--- tests/infra/events/SocketIoAdapter.spec.ts
+++ tests/infra/events/SocketIoAdapter.spec.ts
@@ -3,6 +3,7 @@ import socketIo from "socket.io";
 
 import { EventTypes } from "@/infra/events/EventTypes";
 import { SocketIoAdapter } from "@/infra/events/SocketIoAdapter";
+import { throwError } from "@/tests/domain/mocks";
 
 const makeSut = () => {
     const io = new socketIo.Server();
@@ -18,36 +19,37 @@ const makeSut = () => {
     return { sut, emit, to };
 };
 
+const makeInput = () => ({
+    message: {
+        content: faker.lorem.paragraph(),
+        senderId: faker.datatype.uuid(),
+        sentAt: new Date(),
+    },
+    roomId: faker.datatype.uuid(),
+});
+
 describe("SocketIoAdapter", () => {
     it("should call to() with correct value", () => {
         const { sut, to } = makeSut();
-        const input = {
-            message: {
-                content: faker.lorem.paragraph(),
-                senderId: faker.datatype.uuid(),
-                sentAt: new Date(),
-            },
-            roomId: faker.datatype.uuid(),
-        };
+        const input = makeInput();
         sut.sendMessage(input);
         expect(to).toHaveBeenNthCalledWith(1, input.roomId);
     });
 
     it("should call emit() with correct value", () => {
         const { sut, emit } = makeSut();
-        const input = {
-            message: {
-                content: faker.lorem.paragraph(),
-                senderId: faker.datatype.uuid(),
-                sentAt: new Date(),
-            },
-            roomId: faker.datatype.uuid(),
-        };
+        const input = makeInput();
         sut.sendMessage(input);
         expect(emit).toHaveBeenNthCalledWith(
             1,
             EventTypes.NEW_MESSAGE,
             input.message,
         );
+    });
+
+    it("should throw if emit() throws", () => {
+        const { sut, emit } = makeSut();
+        emit.mockImplementationOnce(throwError);
+        expect(() => sut.sendMessage(makeInput())).toThrowError();
     });
 });
