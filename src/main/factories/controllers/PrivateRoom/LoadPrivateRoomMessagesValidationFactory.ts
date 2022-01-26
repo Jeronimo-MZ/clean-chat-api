138d8fb5986f9153ca6dbfab028bd9c6f9a70551
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-01-26 17:40:31 +0200

    feat: ensure LoadPrivateRoomMessagesValidationFactory makes ValidationComposite with correct validations

---
+++ src/main/factories/controllers/PrivateRoom/LoadPrivateRoomMessagesValidationFactory.ts
@@ -0,0 +1,32 @@
+import { ObjectIdValidatorAdapter } from "@/infra/validators/ObjectIdValidatorAdapter";
+import { Validation } from "@/validation/protocols";
+import {
+    ValidationBuilder,
+    ValidationComposite,
+} from "@/validation/validators";
+
+export const makeLoadPrivateRoomMessagesValidation = (): Validation => {
+    const objectIdValidator = new ObjectIdValidatorAdapter();
+    const userId = ValidationBuilder.field("userId")
+        .required()
+        .objectId(objectIdValidator)
+        .build();
+    const roomId = ValidationBuilder.field("roomId")
+        .required()
+        .objectId(objectIdValidator)
+        .build();
+    const page = ValidationBuilder.field("page")
+        .integer()
+        .min(1, { isNumber: true })
+        .build();
+    const pageSize = ValidationBuilder.field("pageSize")
+        .integer()
+        .min(1, { isNumber: true })
+        .build();
+    return new ValidationComposite([
+        ...userId,
+        ...roomId,
+        ...page,
+        ...pageSize,
+    ]);
+};
--- src/main/factories/controllers/PrivateRoom/index.ts
+++ src/main/factories/controllers/PrivateRoom/index.ts
@@ -1,4 +1,5 @@
 export * from "./AddPrivateRoomController";
 export * from "./AddPrivateRoomValidationFactory";
+export * from "./LoadPrivateRoomMessagesValidationFactory";
 export * from "./SendPrivateMessageControllerFactory";
 export * from "./SendPrivateMessageValidationFactory";
