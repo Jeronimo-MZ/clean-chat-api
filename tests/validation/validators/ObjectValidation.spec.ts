515195630e2f8d95b315fa513818f183ca56fda0
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 16:24:49 +0200

    test: ensure ObjectValidation returns InvalidObjectError if field is not an object

--- tests/validation/validators/ObjectValidation.spec.ts
+++ tests/validation/validators/ObjectValidation.spec.ts
@@ -1,6 +1,7 @@
 import faker from "@faker-js/faker";
 
 import { ValidationSpy } from "@/tests/validation/mocks";
+import { InvalidObjectError } from "@/validation/errors";
 import { ObjectValidation } from "@/validation/validators";
 
 const field = faker.random.word();
@@ -29,9 +30,15 @@ describe("ObjectValidation", () => {
         expect(error).toBeNull();
     });
 
-    it("should return null if field is not provided", () => {
+    it("should return null if no value is given", () => {
         const { sut } = makeSut();
         const error = sut.validate({});
         expect(error).toBeNull();
+    });
+
+    it("should return InvalidObjectError if field is not an object", () => {
+        const { sut } = makeSut();
+        const error = sut.validate({ [field]: faker.datatype.number() });
+        expect(error).toEqual(new InvalidObjectError(field));
     });
 });
