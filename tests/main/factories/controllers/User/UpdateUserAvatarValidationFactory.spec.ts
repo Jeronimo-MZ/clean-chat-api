031fcf89d0fd487c0d3d72d1900a339e784d4c1e
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 19:26:48 +0200

    test: ensure UpdateUserAvatarValidationFactory makes ValidationComposite with correct validations

---
+++ tests/main/factories/controllers/User/UpdateUserAvatarValidationFactory.spec.ts
@@ -0,0 +1,35 @@
+import { ObjectIdValidatorAdapter } from "@/infra/validators";
+import { makeUpdateUserAvatarValidation } from "@/main/factories/controllers";
+import {
+    ValidationBuilder as builder,
+    ValidationComposite,
+} from "@/validation/validators";
+
+describe("UpdateUserAvatarValidation", () => {
+    it("should make ValidationComposite with correct validations", () => {
+        const fileValidations = [
+            ...builder.field("buffer").required().maxFileSize(5).build(),
+            ...builder
+                .field("mimetype")
+                .required()
+                .allowedMimetypes(["jpg", "png"])
+                .build(),
+        ];
+        const objectIdValidator = new ObjectIdValidatorAdapter();
+
+        expect(makeUpdateUserAvatarValidation()).toEqual(
+            new ValidationComposite([
+                ...builder
+                    .field("userId")
+                    .required()
+                    .objectId(objectIdValidator)
+                    .build(),
+                ...builder
+                    .field("file")
+                    .required()
+                    .object(fileValidations)
+                    .build(),
+            ]),
+        );
+    });
+});
