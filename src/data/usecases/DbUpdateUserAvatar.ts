64c01eadb88d8179b0c8ed2564c61ed3fe7a1126
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 12:18:14 +0200

    feat: ensure DbUpdateUserAvatar calls SaveFile with correct values

--- src/data/usecases/DbUpdateUserAvatar.ts
+++ src/data/usecases/DbUpdateUserAvatar.ts
@@ -1,5 +1,6 @@
 import { UUIDGenerator } from "@/data/protocols/cryptography";
 import { LoadUserByIdRepository } from "@/data/protocols/database";
+import { SaveFile } from "@/data/protocols/storage";
 import { UserNotFoundError } from "@/domain/errors";
 import { UpdateUserAvatar } from "@/domain/usecases";
 
@@ -7,12 +8,20 @@ export class DbUpdateUserAvatar implements UpdateUserAvatar {
     constructor(
         private readonly loadUserByIdRepository: LoadUserByIdRepository,
         private readonly uuidGenerator: UUIDGenerator,
+        private readonly saveFile: SaveFile,
     ) {}
     async update({
         userId,
+        file,
     }: UpdateUserAvatar.Input): Promise<UpdateUserAvatar.Output> {
         const user = await this.loadUserByIdRepository.loadById(userId);
-        if (user) this.uuidGenerator.generate();
+        if (user) {
+            const key = this.uuidGenerator.generate();
+            await this.saveFile.save({
+                file: file.buffer,
+                fileName: `${key}.${file.mimeType.split("/")[1]}`,
+            });
+        }
         return new UserNotFoundError();
     }
 }
