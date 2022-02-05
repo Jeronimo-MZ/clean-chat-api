9f058c897d47d6128f46315c9f4004f4353a76d5
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 11:47:07 +0200

    feat: ensure DbUpdateUserAvatar calls UUIDGenerator

--- src/data/usecases/DbUpdateUserAvatar.ts
+++ src/data/usecases/DbUpdateUserAvatar.ts
@@ -1,3 +1,4 @@
+import { UUIDGenerator } from "@/data/protocols/cryptography";
 import { LoadUserByIdRepository } from "@/data/protocols/database";
 import { UserNotFoundError } from "@/domain/errors";
 import { UpdateUserAvatar } from "@/domain/usecases";
@@ -5,11 +6,13 @@ import { UpdateUserAvatar } from "@/domain/usecases";
 export class DbUpdateUserAvatar implements UpdateUserAvatar {
     constructor(
         private readonly loadUserByIdRepository: LoadUserByIdRepository,
+        private readonly uuidGenerator: UUIDGenerator,
     ) {}
     async update({
         userId,
     }: UpdateUserAvatar.Input): Promise<UpdateUserAvatar.Output> {
         await this.loadUserByIdRepository.loadById(userId);
+        this.uuidGenerator.generate();
         return new UserNotFoundError();
     }
 }
