12ba395ab061ed42d0769c5e689a82d73b5def9d
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 11:39:18 +0200

    test: ensure DbUpdateUserAvatar throws if LoadUserByIdRepository throws

--- tests/data/usecases/DbUpdateUserAvatar.spec.ts
+++ tests/data/usecases/DbUpdateUserAvatar.spec.ts
@@ -1,7 +1,7 @@
 import { DbUpdateUserAvatar } from "@/data/usecases";
 import { UserNotFoundError } from "@/domain/errors";
 import { LoadUserByIdRepositorySpy } from "@/tests/data/mocks";
-import { mockUpdateUserAvatarInput } from "@/tests/domain/mocks";
+import { mockUpdateUserAvatarInput, throwError } from "@/tests/domain/mocks";
 
 type SutTypes = {
     sut: DbUpdateUserAvatar;
@@ -31,5 +31,15 @@ describe("DbUpdateUserAvatar", () => {
         loadUserByIdRepositorySpy.result = null;
         const output = await sut.update(mockUpdateUserAvatarInput());
         expect(output).toEqual(new UserNotFoundError());
+    });
+
+    it("should throw if LoadUserByIdRepository throws", async () => {
+        const { sut, loadUserByIdRepositorySpy } = makeSut();
+        jest.spyOn(
+            loadUserByIdRepositorySpy,
+            "loadById",
+        ).mockImplementationOnce(throwError);
+        const promise = sut.update(mockUpdateUserAvatarInput());
+        await expect(promise).rejects.toThrow();
     });
 });
