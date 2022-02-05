8e38f947c23ea30273c2b227cf76ca5bab62ef69
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 11:35:56 +0200

    test: ensure DbUpdateUserAvatar returns UserNotFoundError if LoadUserByIdRepository returns null

--- tests/data/usecases/DbUpdateUserAvatar.spec.ts
+++ tests/data/usecases/DbUpdateUserAvatar.spec.ts
@@ -1,4 +1,5 @@
 import { DbUpdateUserAvatar } from "@/data/usecases";
+import { UserNotFoundError } from "@/domain/errors";
 import { LoadUserByIdRepositorySpy } from "@/tests/data/mocks";
 import { mockUpdateUserAvatarInput } from "@/tests/domain/mocks";
 
@@ -23,5 +24,12 @@ describe("DbUpdateUserAvatar", () => {
         const input = mockUpdateUserAvatarInput();
         await sut.update(input);
         expect(loadUserByIdRepositorySpy.id).toBe(input.userId);
+    });
+
+    it("should return UserNotFoundError if LoadUserByIdRepository returns null", async () => {
+        const { sut, loadUserByIdRepositorySpy } = makeSut();
+        loadUserByIdRepositorySpy.result = null;
+        const output = await sut.update(mockUpdateUserAvatarInput());
+        expect(output).toEqual(new UserNotFoundError());
     });
 });
