952b23721ad15f0e4addafa06bfd56feb69b3b87
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 17:39:22 +0200

    feat: ensure UpdateUserAvatarController integrates with Validation correctly

---
+++ src/presentation/controllers/User/UpdateUserAvatarController.ts
@@ -0,0 +1,32 @@
+import { badRequest, serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+import { Validation } from "@/validation/protocols";
+
+export class UpdateUserAvatarController
+    implements
+        Controller<
+            UpdateUserAvatarController.Request,
+            UpdateUserAvatarController.Response
+        >
+{
+    constructor(private readonly validation: Validation) {}
+    async handle(
+        request: UpdateUserAvatarController.Request,
+    ): Promise<HttpResponse<UpdateUserAvatarController.Response>> {
+        try {
+            const error = this.validation.validate(request);
+            if (error) return badRequest(error);
+            return undefined as any;
+        } catch (error) {
+            return serverError(error as Error);
+        }
+    }
+}
+
+export namespace UpdateUserAvatarController {
+    export type Request = {
+        userId: string;
+        file: { buffer: Buffer; mimetype: string };
+    };
+    export type Response = { avatarUrl: string };
+}
--- src/presentation/controllers/User/index.ts
+++ src/presentation/controllers/User/index.ts
@@ -2,3 +2,4 @@ export * from "./LoginController";
 export * from "./SearchUserByUsernameController";
 export * from "./ShowUserController";
 export * from "./SignUpController";
+export * from "./UpdateUserAvatarController";
