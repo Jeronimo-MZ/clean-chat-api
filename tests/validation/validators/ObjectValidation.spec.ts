8c3232e8aec2e2eec69aaf75cb05f1b75cbc985a
Jerónimo Matavel <mataveljeronimo@gmail.com>
2022-02-05 15:44:29 +0200

    test: ensure ObjectValidation returns null if a valid object is given

---
+++ tests/validation/validators/ObjectValidation.spec.ts
@@ -0,0 +1,31 @@
+import faker from "@faker-js/faker";
+
+import { ValidationSpy } from "@/tests/validation/mocks";
+import { ObjectValidation } from "@/validation/validators";
+
+const field = faker.random.word();
+type SutTypes = {
+    sut: ObjectValidation;
+    validationSpies: ValidationSpy[];
+};
+
+const makeSut = (): SutTypes => {
+    const validationSpies = [
+        new ValidationSpy(),
+        new ValidationSpy(),
+        new ValidationSpy(),
+    ];
+    const sut = new ObjectValidation(field, validationSpies);
+    return {
+        sut,
+        validationSpies,
+    };
+};
+
+describe("ObjectValidation", () => {
+    it("should return null if a valid object is given", () => {
+        const { sut } = makeSut();
+        const error = sut.validate({ [field]: faker.helpers.userCard() });
+        expect(error).toBeNull();
+    });
+});
